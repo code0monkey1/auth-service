e13eac4ecfa9f0254d34d42dc309aea18a70f0cd
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2024-03-17 15:06:50 +0530

    typeorm and env setup

---
+++ src/config/data-source.ts
@@ -0,0 +1,20 @@
+import "reflect-metadata";
+import { DataSource } from "typeorm";
+import { User } from "../entity/User";
+import { Config } from ".";
+
+export const AppDataSource = new DataSource({
+    type: "postgres",
+    host: Config.DB_HOST,
+    port: Number(Config.DB_PORT),
+    username: Config.DB_USERNAME,
+    password: Config.DB_PASSWORD,
+    database: Config.DB_NAME,
+
+    // set synchronize to false in production
+    synchronize: Config.NODE_ENV !== "prod",
+    logging: false,
+    entities: [User],
+    migrations: [],
+    subscribers: [],
+});
--- src/config/index.ts
+++ src/config/index.ts
@@ -1,9 +1,18 @@
 import { config } from "dotenv";
-config();
 
-const { PORT, NODE_ENV } = process.env;
+import path from "path";
+
+config({ path: path.join(__dirname, `../../.env.${process.env.NODE_ENV}`) });
+
+const { PORT, NODE_ENV, DB_HOST, DB_PORT, DB_USERNAME, DB_PASSWORD, DB_NAME } =
+    process.env;
 
 export const Config = {
     PORT,
     NODE_ENV,
+    DB_HOST,
+    DB_PORT,
+    DB_USERNAME,
+    DB_PASSWORD,
+    DB_NAME,
 };
