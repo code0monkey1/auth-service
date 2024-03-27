dc5ba074f8c6f16c0e03893a31b4d2fe255f91f6
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2024-03-27 15:13:55 +0530

    create tenant table

--- src/config/data-source.ts
+++ src/config/data-source.ts
@@ -1,8 +1,6 @@
 import "reflect-metadata";
 import { DataSource } from "typeorm";
-import { User } from "../entity/User";
 import { Config } from ".";
-import { RefreshToken } from "../entity/RefreshToken";
 
 export const AppDataSource = new DataSource({
     type: "postgres",
@@ -14,7 +12,7 @@ export const AppDataSource = new DataSource({
     // keep true only in production
     synchronize: Config.NODE_ENV !== "prod",
     logging: false,
-    entities: [User, RefreshToken],
+    entities: ["src/entity/*.ts"],
     migrations: ["src/migration/*.ts"],
     subscribers: [],
 });
