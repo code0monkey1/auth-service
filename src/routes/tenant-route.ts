317301fe439a42cfe0a764843e607e50c76d1de6
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2024-03-28 14:28:25 +0530

    add test to ensure the user is authenticated, before creating a  new tenant

--- src/routes/tenant-route.ts
+++ src/routes/tenant-route.ts
@@ -1,11 +1,12 @@
 /* eslint-disable @typescript-eslint/no-misused-promises */
 import { Router } from "express";
 import { createTenantControllerFactory } from "./controllerFactory/tenantControllerFactory";
+import authenticate from "../middleware/authenticate";
 
 const route = Router();
 
 const tenantController = createTenantControllerFactory();
 
-route.post("/", tenantController.create);
+route.post("/", authenticate, tenantController.create);
 
 export default route;
--- test/tenants/create.spec.ts
+++ test/tenants/create.spec.ts
@@ -4,19 +4,38 @@ import { AppDataSource } from "../../src/config/data-source";
 import { User } from "../../src/entity/User";
 import setupApp from "../../src/config/app";
 import { Tenant } from "../../src/entity/Tenant";
+import createMockJwks, { JWKSMock } from "mock-jwks";
+import { ROLES } from "../../src/constants";
 
 const app = setupApp();
 const api = supertest(app);
 const BASE_URL = "/tenant";
 
 let connection: DataSource;
+let jwks_server: JWKSMock;
+const JWKS_URI = "http://localhost:3000";
+let authToken: string;
 
 describe("POST /tenant", () => {
     beforeAll(async () => {
         connection = await AppDataSource.initialize();
+        jwks_server = createMockJwks(JWKS_URI);
     });
+
+    beforeEach(async () => {
+        jwks_server.start();
+        await connection.dropDatabase();
+        await connection.synchronize();
+
+        authToken = jwks_server.token({
+            userId: "1",
+            role: ROLES.ADMIN,
+        });
+    });
+
     afterEach(async () => {
         await clearDatabase();
+        jwks_server.stop();
     });
 
     afterAll(async () => {
@@ -40,7 +59,11 @@ describe("POST /tenant", () => {
         };
 
         //act
-        await api.post(BASE_URL).send(tenant).expect(201);
+        await api
+            .post(BASE_URL)
+            .set("Cookie", [`accessToken=${authToken};`])
+            .send(tenant)
+            .expect(201);
     });
     it("should create a tenant in db", async () => {
         // arrange
@@ -54,17 +77,39 @@ describe("POST /tenant", () => {
 
         //act
 
-        await api.post(BASE_URL).send(tenant).expect(201);
+        await api
+            .post(BASE_URL)
+            .set("Cookie", [`accessToken=${authToken};`])
+            .send(tenant)
+            .expect(201);
 
         //assert
         assertCreatesNewTenant(tenant, tenantsBefore);
     });
-    it.todo("should not create a tenant when not set by admin");
+    it("should return 401 if user is not authenticated", async () => {
+        // arrange
+        const tenant = {
+            name: "name",
+            address: "address",
+        };
+        const tenantRepository = connection.getRepository(Tenant);
+
+        //act
+
+        await api.post(BASE_URL).send(tenant).expect(401);
+
+        // assert
+        const tenants = await tenantRepository.find();
+
+        expect(tenants.length).toBe(0);
+    });
 });
 
 async function assertCreatesNewTenant(tenant: any, tenantsBefore: any) {
     const tenantRepository = connection.getRepository(Tenant);
     const tenantsAfter = await tenantRepository.find();
+
+    expect(tenantsAfter.length).toBeGreaterThan(0);
 
     expect(tenantsAfter[tenantsAfter.length - 1].name).toBe(tenant.name);
     expect(tenantsAfter[tenantsAfter.length - 1].address).toBe(tenant.address);
