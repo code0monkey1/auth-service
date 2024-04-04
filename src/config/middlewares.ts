bceb6d879586e3d8ac8e0c4c9ad0e6905a3e2489
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2024-04-04 22:16:34 +0530

    remove client_url not found error

--- src/config/middlewares.ts
+++ src/config/middlewares.ts
@@ -3,16 +3,13 @@ import { bodyParser } from "../main/factories/middlewares/body-parser";
 import { cookieParser } from "../main/factories/middlewares/cookie-parser";
 import cors from "cors";
 import { Config } from ".";
-import createHttpError from "http-errors";
+
 export default (app: Express): void => {
-    if (!Config.CLIENT_URL) {
-        throw createHttpError(400, "Client_Url not found");
-    }
     app.use(bodyParser);
     app.use(cookieParser);
     app.use(
         cors({
-            origin: [Config.CLIENT_URL],
+            origin: [Config.CLIENT_URL!],
             credentials: true,
         }),
     );
